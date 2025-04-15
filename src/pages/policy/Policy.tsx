a3eac743f3f41babe8697d90d5497ef48fa3d538
arzhannikov <artcrazy.mail@gmail.com>
2025-04-15 23:53:53 +0300

    add console.log-2

--- src/pages/policy/Policy.tsx
+++ src/pages/policy/Policy.tsx
@@ -1,6 +1,7 @@
 import PrivacyPolicy from "@/shared/components/PrivacyPolicy";
 
 export default function PrivacyPolicyPage() {
+    console.log('2')
     return (
         <PrivacyPolicy />
     );
