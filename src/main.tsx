53f11032268da6cbe1da9b8ffd11972590ae9dc3
arzhannikov <artcrazy.mail@gmail.com>
2025-04-19 11:36:37 +0300

    Test rules 2

--- src/main.tsx
+++ src/main.tsx
@@ -21,6 +21,8 @@ const PrivateRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => 
   return user ? <>{children}</> : <Navigate to="/login" />;
 };
 
+console.log('test rules 2')
+
 const App = () => {
   const [isAuthModalOpen, setIsAuthModalOpen] = useState(false);
   const [authMode, setAuthMode] = useState<'login' | 'register'>('login');
